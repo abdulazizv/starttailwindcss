c321700b6a05d740ddfff1270fb1b51f36bcb551
Abdulazizv <abdulazizheir@gmail.com>
2023-02-01 11:16:37 +0500

    index.js added

---
+++ src/script/index.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const modalWindow = document.querySelector(".modal-window"),
+  modalContent = document.querySelector(".modal-content"),
+  signUpBtn = document.querySelector(".register-btn"),
+  closeBtn = document.querySelector(".close-btn");
+
+signUpBtn.addEventListener("click", () => {
+  modalWindow.classList.remove("hidden");
+});
+
+closeBtn.addEventListener("click", () => {
+  modalWindow.classList.add("hidden");
+});
+
+window.addEventListener("click", (e) => {
+  console.log(e.target.classList.contains("modal-window"));
+  if (e.target.classList.contains("modal-window")) {
+    modalWindow.classList.toggle("shaker");
+  }
+});
